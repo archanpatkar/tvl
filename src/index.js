dd52d13db10dd2101071ae83a9c63239fa5f1581
archanpatkar <pat@archanpatkar.com>
2019-09-27 09:19:55 +0530

    Moving

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const convert = v => {
+    if(v === true) return 1;
+    else if(v === false) return -1;
+    else if(v === undefined) return 0;
+    throw new Error("Please pass a valid truth value");
+}
+
+const revert = v => {
+    if(v === 1) return true;
+    else if(v === -1) return false;
+    else if(v === 0) return undefined;
+    throw new Error("Please pass a valid number");
+}
+
+const neg = a => -a;
+const min = (a,b) => a > b ? b : b > a ? a : a;
+const max = (a,b) => a > b ? a : b > a ? b : a;
+const not = x => revert(neg(convert(x)));
+const and = (x,y) => revert(min(convert(x),convert(y)));
+const or = (x,y) => revert(max(convert(x),convert(y)));
+const xor = (x,y) => and(or(x,y),not(and(x,y)));
+const imp = (x,y) => or(not(x),y);
+const bi = (x,y) => and(imp(x,y),imp(y,x));
+
+module.exports = {
+    not,
+    and,
+    or,
+    xor,
+    imp,
+    bi,
+    neg,
+    min,
+    max,
+    convert,
+    revert
+};
